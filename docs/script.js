98c6d24293ab40efc49b71b9a44cff832453376a
imamanjangra <imamanjangra@gmail.com>
2025-08-21 21:31:16 +0530

    All done

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,6 @@
+
+//---------------------------start-------------------------------
+
 let btns = document.querySelectorAll(".s");   // saare buttons
 let display = document.querySelector(".display");
 let historyBox = document.querySelector(".history");
@@ -9,13 +12,13 @@ let body = document.querySelector("body");
 let main_box = document.querySelector(".main_box");
 let gr = document.querySelector(".gr");
 
+//-------------------------dark button-------------------
 let dark_value = true
 dark_btn.addEventListener('click' , ()=>{
+
   if(dark_value){
     body.style.backgroundColor = "black";
-
     main_box.style.boxShadow = "0px 0px 20px white";
-
     btns.forEach(i => {
       i.style.backgroundColor = "#141314"
     });
@@ -29,19 +32,19 @@ dark_btn.addEventListener('click' , ()=>{
       })
     });
 
-    gr.style.setProperty("background-color", "#00A300", "important");
-    gr.addEventListener('mouseover' , ()=>{
-     gr.style.setProperty("background-color", "#00A300", "important");
-    })
-    gr.addEventListener('mouseout' , ()=>{
       gr.style.setProperty("background-color", "#00A300", "important");
-    })
+      gr.addEventListener('mouseover' , ()=>{
+        gr.style.setProperty("background-color", "#00A300", "important");
+      })
+      gr.addEventListener('mouseout' , ()=>{
+        gr.style.setProperty("background-color", "#00A300", "important");
+      })
 
     body.style.color = "white";
     dark_value = false;
   }else{
-
     main_box.style.boxShadow = "0 25px 50px -12px rgba(0, 0, 0, 0.25)";
+
     btns.forEach(i => {
       i.style.backgroundColor = "#F6F6F6"
     });
@@ -69,13 +72,17 @@ dark_btn.addEventListener('click' , ()=>{
 
 })
 
+//----------------------calculator for mouse-----------------------------
+
   let exp = "";
   let arr = [];
   let p_arr = [];
+
 btns.forEach(button => {
+
   button.addEventListener('click' , ()=>{
     let value = button.value;
-    console.log(value);
+
      if(exp == ""){
       if(value == "+" || value == "-" || value == "*" || value == "%" || value == "/"   || value == "="){
         alert("Entre number first")
@@ -90,111 +97,98 @@ btns.forEach(button => {
       exp = " ";
       display.innerText = " ";
     } 
+
     else if(value == "%"){
-      console.log(exp);
       let p_value = exp;
       p_arr.push(p_value)
-      console.log(p_arr)
 
         if(exp.includes("*")){
-           let parts = exp.split("*"); 
+            let parts = exp.split("*"); 
             let number = parts[1];
             let f_no = parts[0];
-          console.log(number);
-
-          let o_no = number/100;
-          console.log(o_no);
-          
-          console.log(`${f_no}*${o_no}`);
+            let o_no = number/100;
           display.innerHTML = `${f_no}*${o_no}`;
           exp = `${f_no}*${o_no}`;
-          console.log("*show");
-          
-          return
       }
-      else if(exp.includes("+")){
-        console.log("ok tested");
-         let parts = exp.split("+"); 
-          let number = parts[1];
-           let f_no = parts[0];
+
+        else if(exp.includes("+")){
+            let parts = exp.split("+"); 
+            let number = parts[1];
+            let f_no = parts[0];
             let o_no = number/100;
             let m = o_no*f_no;
-            console.log(`${f_no}+${m}`);
             display.innerHTML = `${f_no}+${m}`;
             exp = `${f_no}+${m}`;
-      }
+        }
+        
       else if(exp.includes("-")){
-        console.log("ok tested");
-         let parts = exp.split("-"); 
+          let parts = exp.split("-"); 
           let number = parts[1];
-           let f_no = parts[0];
-            let o_no = number/100;
-            let m = o_no*f_no;
-            console.log(`${f_no}-${m}`);
-            display.innerHTML = `${f_no}-${m}`;
-            exp = `${f_no}-${m}`;
+          let f_no = parts[0];
+          let o_no = number/100;
+          let m = o_no*f_no;
+          display.innerHTML = `${f_no}-${m}`;
+          exp = `${f_no}-${m}`;
       }
+
       else{
          let last_v = Number(exp);
          let o_value = last_v/100;
-         console.log(o_value);
          display.innerText = o_value;
          exp = o_value;
-
-        
       }
       
-
     }
    
     else if(value == "Backspace"){
-      console.log(exp);
+      
       if(exp == ""){
-       
             return;
-     }else{
-     let arr = exp.split("").map(ch => (isNaN(ch) ? ch : Number(ch)));
-      console.log(arr);
-      arr.pop();
-      console.log(arr);  
-      exp = arr.join("");
-      console.log(exp);
-      display.innerText = exp;
-       }
+     }
+
+      else{
+        let arr = exp.split("").map(ch => (isNaN(ch) ? ch : Number(ch)));
+        arr.pop();  
+        exp = arr.join("");
+        display.innerText = exp;
+      }
     }
+
     else if(value == "="){
       if(exp != " "){
         let ans = eval(exp);
         display.innerText = ans;
         history_fn(exp , ans)
       }
+      
       else{
         alert("Entre a value ");
             return;
       }
-
     }
+
     else{
       exp = exp + value;
       display.innerText = exp;
     }
+
   })
 });
 
 
-// for keybord
+// -----------------------------calculator for keybord --------------------------------
+
 document.addEventListener('keydown', (e)=>{
     let key = e.key;
-    console.log(key);
+
     if(key == "+" || key == "-" || key == "*" || key == "%" || key == "/"|| key == "Enter" || key == "c"  || key == "%" || key == "."  || key == "1" || key == "2" || key == "3" || key == "4" || key == "5" || key == "6" || key == "7" || key == "8" || key == "9" || key == "0" || key == "00" || key == "=" || key == "Entre" || key == "Backspace"  || key == "="){
       run();
     }else{
-     
       return;
     }
+
+
 function run(){
-
-
      if(exp == ""){
       if(key == "+" || key == "-" || key == "*" || key == "%" || key == "/"   || key == "="){
         alert("Entre number first ")
@@ -210,74 +204,56 @@ function run(){
     }
     
     else if(key == "%"){
-      console.log(exp);
       let p_value = exp;
       p_arr.push(p_value)
-      console.log(p_arr)
 
         if(exp.includes("*")){
-           let parts = exp.split("*"); 
-            let number = parts[1];
-            let f_no = parts[0];
-          console.log(number);
-
+          let parts = exp.split("*"); 
+          let number = parts[1];
+          let f_no = parts[0];
           let o_no = number/100;
-          console.log(o_no);
-          
-          console.log(`${f_no}*${o_no}`);
           display.innerHTML = `${f_no}*${o_no}`;
           exp = `${f_no}*${o_no}`;
-          console.log("*show");
-          
-          return
       }
+
       else if(exp.includes("+")){
-        console.log("ok tested");
-         let parts = exp.split("+"); 
+          let parts = exp.split("+"); 
           let number = parts[1];
-           let f_no = parts[0];
-            let o_no = number/100;
-            let m = o_no*f_no;
-            console.log(`${f_no}+${m}`);
-            display.innerHTML = `${f_no}+${m}`;
-            exp = `${f_no}+${m}`;
+          let f_no = parts[0];
+          let o_no = number/100;
+          let m = o_no*f_no;
+          display.innerHTML = `${f_no}+${m}`;
+          exp = `${f_no}+${m}`;
       }
       else if(exp.includes("-")){
-        console.log("ok tested");
-         let parts = exp.split("-"); 
+          let parts = exp.split("-"); 
           let number = parts[1];
-           let f_no = parts[0];
-            let o_no = number/100;
-            let m = o_no*f_no;
-            console.log(`${f_no}-${m}`);
-            display.innerHTML = `${f_no}-${m}`;
-            exp = `${f_no}-${m}`;
+          let f_no = parts[0];
+          let o_no = number/100;
+          let m = o_no*f_no;
+          display.innerHTML = `${f_no}-${m}`;
+          exp = `${f_no}-${m}`;
       }
+
       else{
          let last_v = Number(exp);
          let o_value = last_v/100;
          console.log(o_value);
          display.innerText = o_value;
          exp = o_value;
-
-        
       }
       
-
     }
 
     else if(key == "Backspace"){
       if(exp == ""){
-        
               return;
       }
+
       else{
           let arr = exp.split("").map(ch => (isNaN(ch) ? ch : Number(ch)));
-          console.log(arr);
-          arr.pop();
-          console.log(arr);  
+          arr.pop(); 
           exp = arr.join("");
-          console.log(exp);
           display.innerText = exp;
         }
     }
@@ -303,29 +279,28 @@ function run(){
    }
 
 })
+
 his_icone.addEventListener('click', ()=> {
-  historyBox.classList.remove("hidden");
-  his_icone.classList.add("!hidden")
-  show_icone.classList.remove("!hidden");
-  btnBox.classList.add("hidden");        
+
+    historyBox.classList.remove("hidden");
+    his_icone.classList.add("!hidden")
+    show_icone.classList.remove("!hidden");
+    btnBox.classList.add("hidden");   
+
 });
 
-function history_fn(exp , ans){
-  console.log("fuction is call")
-  console.log(exp);
-  console.log(ans);
-  
+//---------------------------History icone-------------------------------------------
 
+function history_fn(exp , ans){
   
   let p = document.createElement("p");
-   let p_ans = document.createElement("p");
-   p.style.marginLeft = "10px";
-    p.style.fontWeight = "bold";       
-    // p.style.color = "#333";   
+  let p_ans = document.createElement("p");
+  p.style.marginLeft = "10px";
+  p.style.fontWeight = "bold";       
 
   p_ans.style.marginLeft = "25px";
-   p_ans.style.marginBottom = "13spx"
-   p_ans.style.fontStyle = "italic";
+  p_ans.style.marginBottom = "13spx"
+  p_ans.style.fontStyle = "italic";
 
   p.innerText = `• ${exp}`;
   p_ans.innerText = `Ans=${ans}`;
@@ -339,3 +314,5 @@ show_icone.addEventListener('click' , ()=>{
    his_icone.classList.remove("!hidden")
    show_icone.classList.add("!hidden");
 })
+
+//----------------------------End------------------------------------------
